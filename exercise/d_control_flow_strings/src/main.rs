e872ad3ece899946aeb5fb9b6fbb6ec326bb1da2
medhanies <medhaniesolomon@yahoo.com>
2023-04-03 00:25:09 -0500

    exercise d complete

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     //
     // ...produces the equivalent of
     //
-    //     vec!["apple".to_string(), "banana".to_string()]
+        // vec!["apple".to_string(), "banana".to_string()]
     let args: Vec<String> = std::env::args().skip(1).collect();
 
     // This consumes the `args` vector to iterate through each String
@@ -20,6 +20,13 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
+        if arg == "sum" {
+            sum()
+        } else if arg == "double" {
+            double()
+        } else {
+            count(arg)
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -31,7 +38,9 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
+    for i in 7..=23 {
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,6 +51,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
+    while x < 500 {
+        count += 1;
+        x*=2;
+    }
 
 
     println!("You can double x {} times until x is larger than 500", count);
@@ -51,8 +64,14 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut i = 0;
+    loop {
+        while i < 8 {
+            print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+            i += 1;
+        }
+        break;
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
